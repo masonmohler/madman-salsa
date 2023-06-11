abf2e5ca0106c3b71355ad55998ce7f958ad02a1
masonmohler <mdmohler@ualr.edu>
2023-06-11 18:05:23 +0000

    Finished v1

---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $('.fade').slick({
+      infinite: true,
+      dots: true,
+      slidesToShow: 1,
+      autoplay: true,
+      centerMode: true,
+      variableWidth: true
+    });
+  
+});
